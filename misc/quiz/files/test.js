b4a7d2c6335ca2e5c639f00fb725653901f86f65
Stanley <stano5340@gmail.com>
2024-03-02 11:46:59 +0100

    quizzin

---
+++ misc/quiz/files/test.js
@@ -0,0 +1,22 @@
+let qustions = [
+    {
+        "id": 1,
+        "command": "Translate!",
+        "query": "Hallo",
+        "c1": "Hello",
+        "c2": "Bye",
+        "c3": "Please",
+        "c4": "How are you",
+        "c": 1
+    },
+    {
+        "id": 2,
+        "command": "Answer!",
+        "query": "Wie heiﬂt du?",
+        "c1": "Gut",
+        "c2": "Nein",
+        "c3": "Maria",
+        "c4": "Ja",
+        "c": 3
+    }
+];
---
+++ misc/quiz/main.js
@@ -0,0 +1,11 @@
+console.log(q);
+let ids = [];
+
+function randomQuestion() {
+    while (true) {
+        var chosen = questions[Math.floor(Math.random() * questions.length)];
+        if (ids.includes(chosen["id"])) {
+            break
+        }
+    }
+}
