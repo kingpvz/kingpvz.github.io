12dd1ea927923779051658041c348dd3c28fe558
Stanley <109061209+kingpvz@users.noreply.github.com>
2024-03-03 07:17:21 +0000

    more quiz updates

--- misc/quiz/main.js
+++ misc/quiz/main.js
@@ -1,12 +1,44 @@
 let qs = questions[q];
 
+let command = document.getElementById('command');
+let query = document.getElementById('query');
+let c1 = document.getElementById('c1');
+let c2 = document.getElementById('c2');
+let c3 = document.getElementById('c3');
+let c4 = document.getElementById('c4');
+let next = document.getElementById('next');
+let choices = document.getElementById('choices');
+
 let ids = [];
+let correct = 0;
+
+
+function endQuiz() {
+  choices.style.display = 'none';
+}
 
 function randomQuestion() {
     while (true) {
         var chosen = qs[Math.floor(Math.random() * qs.length)];
+      if (ids.length => 10) {
+        break
+        var chosen = {"id": -1}
+      }
         if (!(ids.includes(chosen["id"]))) {
             break
         }
     }
+if (chosen[id] !== -1){
+ids.push(chosen["id"]);
+  command.innerHTML = chosen["command"];
+  query.innerHTML = chosen["query"];
+  c1.innerHTML = chosen["c1"];
+  c2.innerHTML = chosen["c2"];
+  c3.innerHTML = chosen["c3"];
+  c4.innerHTML = chosen["c4"];
+  correct = chosen["c"];
+  next.innerHTML = chosen["n"];
+} else {
+  endQuiz();
+}
 }
--- misc/quiz/storage.js
+++ misc/quiz/storage.js
@@ -1,24 +1,26 @@
 var questions = {
 "test": [
     {
-        "id": 1,
+        "id": 0,
         "command": "Translate!",
         "query": "Hallo",
         "c1": "Hello",
         "c2": "Bye",
         "c3": "Please",
         "c4": "How are you",
-        "c": 1
+        "c": 1,
+        "n": "Next"
     },
     {
-        "id": 2,
+        "id": 1,
         "command": "Answer!",
         "query": "Wie heißt du?",
         "c1": "Gut",
         "c2": "Nein",
         "c3": "Maria",
         "c4": "Ja",
-        "c": 3
+        "c": 3,
+        "n": "Next"
     }
 ], 
 "places": []
