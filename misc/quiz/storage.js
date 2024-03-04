a2538d531d66e30e4948b55def6cdabfff8f87f5
Stanley <109061209+kingpvz@users.noreply.github.com>
2024-03-04 17:21:25 +0000

    storage updated to fit at least 10 elements

--- misc/quiz/storage.js
+++ misc/quiz/storage.js
@@ -7,7 +7,7 @@ var questions = {
         "c1": "slnko",
         "c2": "zóna",
         "c3": "Soňa",
-        "c4": "Nemenovaný učiteľ matematiky je kokot",
+        "c4": "zorna",
         "c": 1,
         "n": "Ďalej"
     },
@@ -18,10 +18,109 @@ var questions = {
         "c1": "Gut.",
         "c2": "Nein.",
         "c3": "Maria.",
-        "c4": "Mathe Lerner ist ein kokot.",
+        "c4": "Zorna.",
         "c": 3,
         "n": "Ďalej"
-    }
+    },
+  {
+    "id": 2,
+    "command": "Question 3",
+    "query": "Query 3",
+    "c1": "c1 3",
+    "c2": "c2 3",
+    "c3": "c3 3",
+    "c4": "c4 3",
+    "c": 2,
+    "n": "Next 3"
+  },
+  {
+    "id": 3,
+    "command": "Question 4",
+    "query": "Query 4",
+    "c1": "c1 4",
+    "c2": "c2 4",
+    "c3": "c3 4",
+    "c4": "c4 4",
+    "c": 4,
+    "n": "Next 4"
+  },
+  {
+    "id": 4,
+    "command": "Question 5",
+    "query": "Query 5",
+    "c1": "c1 5",
+    "c2": "c2 5",
+    "c3": "c3 5",
+    "c4": "c4 5",
+    "c": 1,
+    "n": "Next 5"
+  },
+  {
+    "id": 5,
+    "command": "Question 6",
+    "query": "Query 6",
+    "c1": "c1 6",
+    "c2": "c2 6",
+    "c3": "c3 6",
+    "c4": "c4 6",
+    "c": 2,
+    "n": "Next 6"
+  },
+  {
+    "id": 6,
+    "command": "Question 7",
+    "query": "Query 7",
+    "c1": "c1 7",
+    "c2": "c2 7",
+    "c3": "c3 7",
+    "c4": "c4 7",
+    "c": 3,
+    "n": "Next 7"
+  },
+  {
+    "id": 7,
+    "command": "Question 8",
+    "query": "Query 8",
+    "c1": "c1 8",
+    "c2": "c2 8",
+    "c3": "c3 8",
+    "c4": "c4 8",
+    "c": 4,
+    "n": "Next 8"
+  },
+  {
+    "id": 8,
+    "command": "Question 9",
+    "query": "Query 9",
+    "c1": "c1 9",
+    "c2": "c2 9",
+    "c3": "c3 9",
+    "c4": "c4 9",
+    "c": 1,
+    "n": "Next 9"
+  },
+  {
+    "id": 9,
+    "command": "Question 10",
+    "query": "Query 10",
+    "c1": "c1 10",
+    "c2": "c2 10",
+    "c3": "c3 10",
+    "c4": "c4 10",
+    "c": 2,
+    "n": "Next 10"
+  },
+  {
+    "id": 10,
+    "command": "Question 11",
+    "query": "Query 11",
+    "c1": "c1 11",
+    "c2": "c2 11",
+    "c3": "c3 11",
+    "c4": "c4 11",
+    "c": 3,
+    "n": "Next 11"
+  }
 ], 
 "places": []
 };
