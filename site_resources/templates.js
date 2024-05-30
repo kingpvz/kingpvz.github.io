4725b1795f3bc4320d78cb8cd3676f9d4a1a300b
Stanley <stano5340@gmail.com>
2024-05-30 17:08:39 +0200

    Removed discord iframe. More in DSC - Added discord server link to the header

--- site_resources/templates.js
+++ site_resources/templates.js
@@ -9,7 +9,8 @@ headertemplate.innerHTML = `
             <a target="_blank" style="color: #FF424D;" class="titleitem" href="https://patreon.com/kingpvz">Patreon</a><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
             <a target="_blank" style="color: #F56040" class="titleitem" href="https://instagram.com/kingpvzyt">Instagram</a><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
             <a target="_blank" style="color: #1DB954;" class="titleitem" href="https://open.spotify.com/artist/0E9m4Xiqd4g7gAnByJ8FZW?si=1m-T9AhcS7-npeM3t_ZHCw">Spotify</a><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
-            <a target="_blank" style="color: white;" class="titleitem" href="https://my-store-b3cfd9.creator-spring.com/">Merch</a>
+            <a target="_blank" style="color: white;" class="titleitem" href="https://my-store-b3cfd9.creator-spring.com/">Merch</a><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+            <a target="_blank" style="color: #ccf;" class="titleitem" href="https://discord.gg/dSA8SjbkXP">Discord Server</a>
          </div></div>
 `;
 
