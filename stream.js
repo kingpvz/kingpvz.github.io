aac79a04c38ca6edb10bf805d6d58f7b78e77f5c
Stanley <stano5340@gmail.com>
2026-02-11 17:16:58 +0100

    gdfdfgdfg

--- stream.js
+++ stream.js
@@ -41,7 +41,7 @@ window.onload = function () {
         }
         return map;
     }
-    const TZ = 'Europe/Berlin';
+    const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
     for (i of receive.schedule) {
         const body = document.createElement("div");
@@ -50,19 +50,32 @@ window.onload = function () {
         const date = document.createElement("h2");
         let td = new Date();
         const dtParts = getPFTZ(Date.parse(i[0]), 'Europe/Berlin');
-        const nowParts = getPFTZ(td, Intl.DateTimeFormat().resolvedOptions().timeZone);
+        const nowParts = getPFTZ(td, tz);
 
         const dtKey = `${dtParts.year}-${dtParts.month}-${dtParts.day}`;
         const nowKey = `${nowParts.year}-${nowParts.month}-${nowParts.day}`;
-
         const nowMinutes = parseInt(nowParts.hour, 10) * 60 + parseInt(nowParts.minute, 10);
-
         if (dtKey === nowKey) {
             if (nowMinutes < 16 * 60) date.innerHTML = "Today";
-            else if (nowMinutes < 18 * 60) date.innerHTML = "Live Now"
+            else if (nowMinutes < 18 * 60) date.innerHTML = "Live Now";
             else date.innerHTML = "Finished";
         } else if (dtKey > nowKey) {
-            date.innerHTML = "Future";
+            const dtMidUtc = Date.UTC(+dtParts.year, +dtParts.month - 1, +dtParts.day);
+            const nowMidUtc = Date.UTC(+nowParts.year, +nowParts.month - 1, +nowParts.day);
+            const dayDiff = Math.round((dtMidUtc - nowMidUtc) / (24 * 60 * 60 * 1000));
+
+            if (dayDiff === 1) {
+                date.innerHTML = "Tomorrow";
+            } else if (dayDiff > 1) {
+                const userLocale = navigator.language || 'en-US';
+                const shortFormatter = new Intl.DateTimeFormat(userLocale, {
+                    timeZone: tz,
+                    month: '2-digit',
+                    day: '2-digit'
+                });
+                const shortDate = shortFormatter.format(Date.parse(i[0]));
+                date.innerHTML = shortDate;
+            }
         } else {
             date.innerHTML = "Finished";
         }
